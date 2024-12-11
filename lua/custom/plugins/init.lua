561727017e13b9c7ce467bf888aaa47d9632c44e
5brian <brian.tan88@gmail.com>
2024-12-11 07:46:45 -0500

    refactor: 5brian/quandary-nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,30 +3,30 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-        'goolord/alpha-nvim',
-        -- dependencies = { 'echasnovski/mini.icons' },
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-          local startify = require 'alpha.themes.startify'
-          -- available: devicons, mini, default is mini
-          -- if provider not loaded and enabled is true, it will try to use another provider
-          startify.file_icons.provider = 'devicons'
-          require('alpha').setup(startify.config)
-        end,
-      },
-      {
-        'ThePrimeagen/harpoon',
-        branch = 'harpoon2',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-          'nvim-tree/nvim-web-devicons', -- optional
-        },
-      },
-      {
-        'yoleuh/quandary-nvim',
-        ft = { 'quandary' },
-        config = true,
-      },
+  {
+    'goolord/alpha-nvim',
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require 'alpha.themes.startify'
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = 'devicons'
+      require('alpha').setup(startify.config)
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
+  {
+    '5brian/quandary-nvim',
+    ft = { 'quandary' },
+    config = true,
+  },
 }
